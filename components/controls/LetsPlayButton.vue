55a227c594c815b5e3f653d62c21bb91a121ca11
Jeremy <me@jeremypoole.ca>
2021-07-19 21:11:36 -0700

    Work-in-progress branch. For some reason I was factoring out the 'lets play' button into it's own component. I think I was adding tabs to the home page for a settings screen, but it's been so long I can't remember.

---
+++ components/controls/LetsPlayButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <div :class="{ fadeOut }" class="play">
+    <a
+      @click.prevent="
+        () => {
+          fadeOut = true
+          setActiveIndex(0, () => {
+            $router.push('/categories')
+          })
+        }
+      "
+      :class="{ hover: activeIndex === 0 }"
+      class="button--green"
+      href="/categories"
+      >Let's play!</a
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LetsPlayButton',
+  data() {
+    return {
+      fadeOut: false
+    }
+  },
+  methods: {
+    clickHandler() {
+      this.fadeOut = true
+      this.$emit('clicked')
+    }
+  }
+}
+</script>
